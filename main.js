64c196ad5b884afca70b66e17dbdd2d6da2a86b3
raianebn <bonfim.raiane@escola.pr.gov.br>
2026-06-17 11:59:58 -0300

    .

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-// Acessando os elementos através do DOM
+// Acessando os elementos atraves do DOM
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 const botaoMenos = document.querySelector('.botao-menos');
 const botaoMais = document.querySelector('.botao-mais');
 
-// Criando a variável para controlar o valor atual do tamanho da senha
+// Criando a variavel para controlar o valor atual do tamanho da senha
 let tamanhoSenha = 12;
 
-// Função para atualizar o número que aparece na tela
+// Funcao para atualizar o numero que aparece na tela
 function atualizarTexto() {
     numeroSenha.textContent = tamanhoSenha;
 }
 
-// Evento para diminuir o número ao clicar no botão "-"
+// Evento para diminuir o numero ao clicar no botao "-"
 botaoMenos.onclick = function() {
-    if (tamanhoSenha > 1) { // Impede que o número seja menor que 1
+    if (tamanhoSenha > 1) { // Impede que o numero seja menor que 1
         tamanhoSenha = tamanhoSenha - 1;
         atualizarTexto();
     }
 };
 
-// Evento para aumentar o número ao clicar no botão "+"
+// Evento para aumentar o número ao clicar no botao "+"
 botaoMais.onclick = function() {
-    if (tamanhoSenha < 20) { // Limita o tamanho máximo em 20 caracteres (opcional)
+    if (tamanhoSenha < 20) { // Limita o tamanho maximo em 20 caracteres (opcional)
         tamanhoSenha = tamanhoSenha + 1;
         atualizarTexto();
     }
